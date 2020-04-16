8987ca2986f8c50ab6667bd4d063feda46956104
HollosJ <hollosj99@gmail.com>
2020-04-16 14:51:59 +0100

    Replaced search system with dropdown menu.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const container = document.getElementById("news_container");
-
 const cnt_flag = document.getElementById("flag");
 const cnt_name = document.getElementById("country_name");
 const cnt_alpha2 = document.getElementById("alpha2");
@@ -10,41 +8,45 @@ const cnt_subreg = document.getElementById("country_subreg");
 const cnt_cptl = document.getElementById("country_cptl");
 const cnt_pop = document.getElementById("country_pop");
 
-const errorBox = document.getElementById("errorBox");
-
-const search = document.getElementById("input");
+const dropDown = document.getElementById("dropSearch");
 const submit = document.getElementById("submit");
 
 let url = "https://restcountries.eu/rest/v2/alpha/";
+let allUrl = "https://restcountries.eu/rest/v2/all";
 
+//WRITE ALL COUNTRIES TO DROP DOWN
+initDropdown = () => {
+  fetch(allUrl)
+    .then((res) => res.json())
+    .then((out) => {
+      for (let i = 0; i < out.length; i++) {
+        let newOption = document.createElement("option");
+        newOption.value = out[i].alpha3Code;
+        newOption.innerText = out[i].name;
+        dropDown.appendChild(newOption);
+      }
+    });
+};
+
+//LISTEN FOR SEARCH CLICK
 submit.addEventListener("click", () => {
-  if (!search.value) {
-    error();
-  } else {
-    fetch(url + search.value)
-      .then(res => res.json())
-      .then(out => {
-        if (!out.status) {
-          //Changing HTML contents
-          cnt_flag.src = out.flag;
-          cnt_name.innerHTML = out.name;
-          cnt_alpha2.innerHTML = out.alpha2Code;
-          cnt_alpha3.innerHTML = out.alpha3Code;
-          cnt_tld.innerHTML = out.topLevelDomain[0];
-          cnt_reg.innerHTML = out.region + ": ";
-          cnt_subreg.innerHTML = out.subregion;
-          cnt_cptl.innerHTML = out.capital;
-          cnt_pop.innerHTML = out.population;
-        } else {
-          //Display error
-          errorBox.classList.remove("hidden");
-          errorBox.classList.add("showing");
-          window.setTimeout(() => {
-            errorBox.classList.add("hidden");
-            errorBox.classList.remove("showing");
-          }, 6000);
-        }
-      })
-      .catch(err => console.error(err));
-  }
+  let selected = dropDown.options[dropDown.selectedIndex].value;
+
+  fetch(url + selected)
+    .then((res) => res.json())
+    .then((out) => {
+      //Changing HTML contents
+      cnt_flag.src = out.flag;
+      cnt_name.innerHTML = out.name;
+      cnt_alpha2.innerHTML = out.alpha2Code;
+      cnt_alpha3.innerHTML = out.alpha3Code;
+      cnt_tld.innerHTML = out.topLevelDomain[0];
+      cnt_reg.innerHTML = out.region + ": ";
+      cnt_subreg.innerHTML = out.subregion;
+      cnt_cptl.innerHTML = out.capital;
+      cnt_pop.innerHTML = out.population;
+    })
+    .catch((err) => console.error(err));
 });
+
+initDropdown();
